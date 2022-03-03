b010593cd8b572f9485162242b6ae8f9c7f84f81
DevelopJKong <ybaram40@gmail.com>
2022-03-03 10:48:32 +0900

    react-hook-form 사용완료 체크포인트

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -28,47 +28,94 @@ import { useForm } from "react-hook-form";
 //   );
 // }
 
-function ToDoList() {
-  const { register, handleSubmit,formState} = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
-  };
-  console.log(formState);
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
 
+function ToDoList() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    //setError("extraError", { message: "Server offline" });
+  };
   return (
     <div>
-      <form style={{display:"flex",flexDirection:"column"}} onSubmit={handleSubmit(onValid)}>
-        email
-        <input {...register("email", { required: true })} placeholder="Email" />
-        first name
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
-          {...register("firstName", { required: true })}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+            },
+          })}
           placeholder="First Name"
         />
-        last name
+        <span>{errors?.firstName?.message}</span>
         <input
-          {...register("lastName", { required: true })}
+          {...register("lastName", { required: "write here" })}
           placeholder="Last Name"
         />
-        username
+        <span>{errors?.lastName?.message}</span>
         <input
-          {...register("username", { required: true, minLength: 10 })}
+          {...register("username", { required: "write here", minLength: 10 })}
           placeholder="Username"
         />
-        password
+        <span>{errors?.username?.message}</span>
         <input
-          {...register("password", { required: true, minLength: 5 })}
+          {...register("password", { required: "write here", minLength: 5 })}
           placeholder="Password"
         />
-        password1
+        <span>{errors?.password?.message}</span>
         <input
-          {...register("password1", { required: "Password is required", minLength: 5 })}
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
           placeholder="Password1"
         />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
   );
 }
-
 export default ToDoList;
