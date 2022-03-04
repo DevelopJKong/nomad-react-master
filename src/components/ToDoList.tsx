b4480650b3c9f2a89ec9ca205cccf07819d8e892
DevelopJKong <ybaram40@gmail.com>
2022-03-04 10:35:17 +0900

    #5.15 Immutability part Two 완료

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -10,7 +10,7 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoState);
   // const value = useRecoilValue(toDoState);
   // const modFn = useSetRecoilState(toDoState);
- 
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
