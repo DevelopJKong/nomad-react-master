6c2f97f5b7d15633e698e5ad952300ec1c53eade
DevelopJKong <ybaram40@gmail.com>
2022-03-05 10:19:08 +0900

    #5.17 Selectors part Two 완료

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,23 +1,30 @@
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import React from "react";
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
+import { categoryState, toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
-
-
-
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
   // const value = useRecoilValue(toDoState);
   // const modFn = useSetRecoilState(toDoState);
-  console.log(toDos);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category,setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
-      <h1>To Dos</h1>
-        <CreateToDo />    
-      <ul>
-        {toDos.map(toDo => <ToDo {...toDo}/>)}
-      </ul>
+      <h2>To Do</h2>
+      <hr />
+        <select value={category} onInput={onInput}>
+          <option value="TO_DO">To Do</option>
+          <option value="DOING">Doing</option>
+          <option value="DONE">Done</option>
+        </select>
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
