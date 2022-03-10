9139a7c8990d1fae114ff9d522a985f7cd375c1b
DevelopJKong <ybaram40@gmail.com>
2022-03-10 19:09:08 +0900

    #7.0 Introduction 완료

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#3F8Cf2",
-  boardColor: "#DADFE9",
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
